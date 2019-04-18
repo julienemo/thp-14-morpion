9f903a01765539a571ad97c2b39ff675364e1285
dhiyabak <dhiyabak@gmail.com>
2019-04-18 13:08:45 +0200

    App

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'bundler'
+Bundler.requirer
+
+
+
+class Application
+  def perform
+    # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
+    # 
+
+  end
+
+end
