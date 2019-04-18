643c623e6ba104c72d9bd7ea6354f9e0eaf8f35f
forkhembo <forkhembo@riseup.net>
2019-04-18 13:01:37 +0200

    show

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -6,7 +6,7 @@ require_relative "player"
 class Board
   #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
   #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
-
+  attr_accessor :case_array
 
   def initialize
     case_array = []
