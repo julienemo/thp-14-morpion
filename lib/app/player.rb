079575f79cac4be8c955dab41f9ff6451e863572
Julie K <juliette.nada@gmail.com>
2019-04-18 12:23:16 +0200

    player : initialize with user input name

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -5,11 +5,17 @@ class Player
   attr_accessor :name
   attr_reader :sign
 
-  def initialize(name, sign)
-    @name = name
+  def initialize(sign)
+    print "> "
+    @name = gets.chomp.capitalize
     @sign = sign
     puts "Player #{@name} created, your sign is #{@sign}."
   end
 end
 
-#player1 = Player.new("Bidule", "x")
+#array = ['O','X']
+#players = []
+#array.each do |sign|
+#  players << Player.new(sign)
+#end
+#print players
