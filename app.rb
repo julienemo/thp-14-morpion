83a6028ba01e31d86e0e8d455b6e55dee7349224
dhiyabak <dhiyabak@gmail.com>
2019-04-18 13:40:29 +0200

    app file modified

--- app.rb
+++ app.rb
@@ -1,6 +1,45 @@
 require 'bundler'
 Bundler.requirer
 
+class App
+  def initialize
+    welcome
+    puts "notes Julie : ici il y aura le jeu"
+    puts "notes Julie : et on va demande au jouer si une autre partie"
+    end_prog
+  end
+
+  private
+  def welcome
+    puts "   "+"-" * 33
+    puts "   |  Welcome to the MORPION game  |"
+    puts "   |                               |"
+    puts "   |     Proudly presented by      |"
+    puts "   |                               |"
+    puts "   |    Dhyia, Julie and Romain    |"
+    puts "   "+"-" * 33
+  end
+
+  def end_prog
+    print "> "
+    choice = gets.chomp.downcase
+    puts choice
+    case choice
+    when ( "q" ||  "quit")
+      puts "You're right, it's time. Seeya soon though !"
+      exit
+    when ( "c" ||  "continue")
+      puts "Of course, anytime."
+      puts "notes Julie : Mets une autre partie en boucle"
+    else
+      puts "Come again ? Didn't get that..."
+      puts "<Q> to quit or <C> to continue"
+      puts "notes Julie :Je gere pas ce genre de boucle au secours"
+    end
+  end
+end
+
+App.new
 
 
 class Application
