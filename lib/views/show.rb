eceba5020055889cdff49589312d16c3765942a1
forkhembo <forkhembo@riseup.net>
2019-04-18 15:00:15 +0200

    show status

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -6,17 +6,28 @@ require_relative '../app/board'
 class Show
  #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
 
-  def initialize(board)
-    @board = board
+  def initialize 
+    @board = Board.new
   end
 
-  def show_board
-board_case = @board.case_array
+  def show_menu
+    board_case = @case_array
+    puts " #{board_case[0].index} | #{board_case[1].index} | #{board_case[2].index} "
+    puts "-----------"
+    puts " #{board_case[3].index} | #{board_case[4].index} | #{board_case[5].index} "
+    puts "-----------"
+    puts " #{board_case[6].index} | #{board_case[7].index} | #{board_case[8].index} " 
+  end
 
-puts " #{board_case[0]} | #{board_case[1]} | #{board_case[2]} "
-puts "-----------"
-puts " #{board_case[3]} | #{board_case[4]} | #{board_case[5]} "
-puts "-----------"
-puts " #{board_case[6]} | #{board_case[7]} | #{board_case[8]} " 
+  def show_status
+    board_case = @board.case_array
+    puts " #{board_case[0].content} | #{board_case[0].content} | #{board_case[0].content}"
+    puts "-----------"
+    puts " #{board_case[0].content} | #{board_case[0].content} | #{board_case[0].content}"
+    puts "-----------"
+    puts " #{board_case[0].content} | #{board_case[0].content} | #{board_case[0].content}" 
   end
 end
+
+ show = Show.new
+ show.show_status
