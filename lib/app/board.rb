19cb9601a1d1b940d99c0e8672e11a781b304c25
dhiyabak <dhiyabak@gmail.com>
2019-04-18 16:32:41 +0200

    board modified

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -25,21 +25,48 @@ class Board
 
   def play_turn(player, index)
     @case_array[index].active(player.sign)
+    player_choice = []
+	puts "Please choose a case:"
+	player_choice = gets.chomp
+	
     #TO DO : une méthode qui :
     #1) demande au bon joueur ce qu'il souhaite faire
     #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
   end
+  
+  private
 
-  def victory?
-  	win_combinations = [
-  		[0,1,2], # Top row
-  		[3,4,5], # Middle row
-  		[6,7,8],
-  		[0,3,6],
-  		[1,4,7],
-  		[2,5,8],
-  		[0,4,8],
-  		[2,4,6]]
-#TO DO : une méthode qui vérifie le plateau et indique s'il y a un vainqueur ou match nul
+  def line?(array, a, b, c)
+  	if (array[a] == array[b]) && (array[a] == array[c]) && array[a] != 0 
+  		return true
+  	else
+  		return false
+  	end
   end
+
+  def one_line?(array)
+  	if line?(array, 0, 1, 2) || line?(array, 3, 4, 5) || line?(array, 6, 7, 8) || line?(array, 0, 3, 6) || line?(array, 1, 4, 7) || line?(array, 2, 5, 8) || line?(array, 0, 4, 8) || line?(array, 2, 4, 6)
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+  def board_full?(array)
+  	if array.join !=~ /\d/
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+  def continue?(array)
+  	if board_full?(array) || one_line?(array)
+  		return false
+  	else
+  		return true
+  	end
+  end
+
+
 end
