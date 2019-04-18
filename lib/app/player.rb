b87f08d0ce7fea925bb964edcc9fce8f304d806c
Julie K <juliette.nada@gmail.com>
2019-04-18 11:27:59 +0200

    player.rb created

---
+++ lib/app/player.rb
@@ -0,0 +1,15 @@
+require 'bundler'
+Bundler.require
+
+class Player
+  attr_accessor :name
+  attr_reader :sign
+
+  def initialize(name, sign)
+    @name = name
+    @sign = sign
+    puts "Player #{@name} created, your sign is #{@sign}."
+  end
+end
+
+player1 = Player.new("Bidule", "x")
